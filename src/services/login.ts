262e048250df85574c9cea40929b765d18a37ab8
ziiizar <cesarfpna@gmail.com>
2024-11-09 10:49:02 -0500

    Update login.ts

--- src/services/login.ts
+++ src/services/login.ts
@@ -26,3 +26,21 @@ export const login = async (data: TSLoginSchema) => {
     return { error: "Login failed:" };
   }
 };
+
+
+// import { axiosInstance } from "@/lib/utils";
+// import { TSLoginSchema } from "@/schemas/user";
+
+// export const login = async (data: TSLoginSchema) => {
+//   const { password, username } = data;
+
+//   try {
+//     const response = await axiosInstance.post("login", { password, username });
+
+//     if (!response) throw new Error("Error during login");
+
+//     return { success: "Login successful" };
+//   } catch (error) {
+//     return { error: "Login failed:" };
+//   }
+// };
